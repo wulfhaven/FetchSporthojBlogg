a8da6f60a0c72ee313ac31787e9c1a723befd7d3
ulvhamne <wulfhaven@gmail.com>
2014-11-17 20:06:11 +0100

    Working function to get all blog urls.

--- download_blog.py
+++ download_blog.py
@@ -3,6 +3,8 @@ import sys
 import requests
 import bs4
 
+LINK_PREFIX = 'http://www.sporthoj.com'
+
 __author__ = 'ulvhamne'
 
 post_links = []
@@ -12,23 +14,35 @@ def fetch_all_post_urls(url: string) -> []:
     page = requests.get(url)
     soup = bs4.BeautifulSoup(page.text)
     hrefs = soup.find_all(id='inlagg')[1].find_all(href=True)
-    post_links.extend([href.attrs.get('href') for href in hrefs])
+    post_links.extend([LINK_PREFIX + href.attrs.get('href') for href in hrefs])
     return [href.attrs.get('href') for href in hrefs]
 
 
-def fetch_all_blog_urls(url: string) -> []:
-    page = requests.get(url)
+def get_all_blog_urls(url: string) -> []:
+    blog_urls = [url, ]
+    initial_page = requests.get(url)
+    initial_soup = bs4.BeautifulSoup(initial_page.text)
+    post_link = initial_soup.find_all(id='inlagg')[1].find_all(href=True)[0].attrs.get('href')
+    page = requests.get(LINK_PREFIX + post_link)
     soup = bs4.BeautifulSoup(page.text)
-    post_link = soup.find_all(id='inlagg')[1].find_all(href=True)[0].attrs.get('href')
-    requests.get(post_link)
+    hrefs = soup.find_all(id='garage')[0].find_all(href=True)
+    blog_urls.extend([LINK_PREFIX + href.attrs.get('href') for href in hrefs])
+    return blog_urls
 
-    return list
+
+def fetch_all_blog_posts(url: string):
+    pass
 
 
 def main(argv):
     print("Scraping all blog entries from: " + argv[0])
+    blog_urls = get_all_blog_urls(argv[0])
+    print("Will read these urls: ")
+    for url in blog_urls:
+        print("  " + url)
 
-    post_links.extend(fetch_all_post_urls(argv[0]))
+    for url in blog_urls:
+        fetch_all_blog_posts(url)
 
 
 if __name__ == "__main__":
