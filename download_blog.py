f32a0b640088bebcc5fdb8d63781cbdd2b109267
ulvhamne <wulfhaven@gmail.com>
2014-11-19 23:13:55 +0100

    Finished purely functionally at least. Could be optimised a bit by pulling the first page when reading blog urls, that way we wont have to load it twice... However, the pulling of images makes this a sub optimisation.

--- download_blog.py
+++ download_blog.py
@@ -1,22 +1,17 @@
 import datetime
+from os import makedirs, chdir
 import re
 import string
 import sys
 import requests
 import bs4
-from twisted.web.resource import Resource
+from bs4 import Tag
 
 LINK_PREFIX = 'http://www.sporthoj.com'
+IMG_URL = 'http://www.sporthoj.com/galleri/bild?id={0}&header=1'
+
 
 post_links = []
-
-
-def fetch_all_post_urls(url: string) -> []:
-    page = requests.get(url)
-    soup = bs4.BeautifulSoup(page.text)
-    hrefs = soup.find_all(id='inlagg')[1].find_all(href=True)
-    post_links.extend([LINK_PREFIX + href.attrs.get('href') for href in hrefs])
-    return [href.attrs.get('href') for href in hrefs]
 
 
 def get_all_blog_urls(url: string) -> []:
@@ -31,23 +26,55 @@ def get_all_blog_urls(url: string) -> []:
     return blog_urls
 
 
-def fetch_all_data(post: Resource):
-    title = post.find(style='font-size: 18px').text
+def download_images(post: Tag) -> None:
     image_links = [LINK_PREFIX + link.attrs.get('href') for link in post.find_all(href=re.compile('/galleri/visabild'))]
-    text = post.find(style='font-size: 13px').text
+    for url in image_links:
+        image_id = re.search(r'id=([0-9]+)[&]', url).group(1)
+        print('    Downloading file: ' + IMG_URL.format(image_id))
+        r = requests.get(IMG_URL.format(image_id), stream=True)
+        with open(image_id + '.jpg', 'wb') as fd:
+            for chunk in r.iter_content(128):
+                fd.write(chunk)
+
+
+def grab_post_datetime(post: Tag) -> string:
     time_text = post.find(id='publicerad').text
     date_time_string = re.search(r'Tidpunkt:(.+)[|]', time_text).group(1).strip()
     post_datetime = datetime.datetime.strptime(date_time_string, '%d / %m - %Y %H:%M').strftime('%y-%m-%d_%H:%M')
+    return post_datetime
+
+
+def clean_filename(name: string) -> string:
+    trans = str.maketrans(string.whitespace, '_' * len(string.whitespace), '/\\.?*%:|"><')
+    return name.translate(trans)
+
+
+def fetch_all_data(post: Tag):
+    title = post.find(style='font-size: 18px').text
+    post_datetime = grab_post_datetime(post)
+    file_name = clean_filename(post_datetime + '-' + title)
+    print('  Creating folder: ' + file_name)
+    makedirs(file_name)
+    chdir(file_name)
+    download_images(post)
+    text = post.find(style='font-size: 13px').text
+    print('    Saving post text')
+    with open('post_text.txt', 'w+', encoding='utf-8') as post_text_file:
+        post_text_file.write(text)
+    chdir('..')
 
 
 def fetch_all_blog_posts(url: string):
     page = requests.get(url)
     full_soup = bs4.BeautifulSoup(page.text)
+    blog_title = full_soup.find(style='font-size: 26px').text
+    file_name = clean_filename(blog_title)
+    print('Creating folder: ' + file_name)
+    makedirs(file_name)
+    chdir(file_name)
     for post in full_soup.find_all(id='inlagg')[0].children:
         fetch_all_data(post)
-
-
-
+    chdir('..')
 
 
 def main(argv):
@@ -58,6 +85,7 @@ def main(argv):
         print("  " + url)
 
     for url in blog_urls:
+        print('Fetching :' + url)
         fetch_all_blog_posts(url)
 
 
